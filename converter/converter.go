9ade03abf17b4da13caf5af6d2b1e856fc1bb1ee
TruthHun <TruthHun@QQ.COM>
2018-02-03 08:16:30 +0800

    first commit

---
+++ converter/converter.go
@@ -0,0 +1,34 @@
+//Author:TruthHun
+//Email: TruthHun@QQ.COM
+//Date:  2018-02-03
+package converter
+
+var tags = map[string]string{
+	"hr":         "- - -\n\n",
+	"br":         "\n",
+	"title":      "# ",
+	"h1":         "# ",
+	"h2":         "## ",
+	"h3":         "### ",
+	"h4":         "#### ",
+	"h5":         "##### ",
+	"h6":         "###### ",
+	"b":          "**",
+	"strong":     "**",
+	"i":          "_",
+	"em":         "_",
+	"dfn":        "_",
+	"var":        "_",
+	"cite":       "_",
+	"ul":         "* ",
+	"ol":         "1. ",
+	"dl":         "- ",
+	"pre":        "```",
+	"blockquote": "> ",
+	"code":       "",
+	"span":       "",
+}
+
+func Convert(htmlstr string) {
+
+}
